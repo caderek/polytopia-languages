5a599255e3f15de2879ff3e4a1d22528864b0e70
Maciej Caderek <maciej.caderek@gmail.com>
2021-12-18 18:28:12 +0100

    Improved lang check script

--- scripts/check-completeness.js
+++ scripts/check-completeness.js
@@ -1,26 +1,35 @@
-const fs = require("fs")
-const lang = process.argv[2]
+const fs = require("fs");
+const lang = process.argv[2];
 
-const original = require("../original.json")
-const translation = require(`../translations/${lang}.json`)
+const original = require("../original.json");
+const translation = require(`../translations/${lang}.json`);
 
-const originalKeys = Object.keys(original)
-const translationKeys = new Set(Object.keys(translation))
+const originalKeys = Object.keys(original);
+const translationKeys = new Set(Object.keys(translation));
 
-const missingKeys = []
+const missingKeys = [];
 
 for (const key of originalKeys) {
   if (translationKeys.has(key)) {
-    continue
+    continue;
   }
 
-  missingKeys.push(key)
+  missingKeys.push(key);
 }
 
-let missing = ""
+if (missingKeys.length === 0) {
+  console.log(`Language ${lang.toUpperCase()} complete!`);
+  process.exit();
+}
+
+console.log(
+  `Language ${lang.toUpperCase()} incomplete, see: ./missing.temp.txt`
+);
+
+let missing = "";
 
 for (const key of missingKeys) {
-  missing += `"${key}": "${original[key]}",\n`
+  missing += `"${key}": "${original[key]}",\n`;
 }
 
-fs.writeFileSync("missing.temp.txt", missing)
+fs.writeFileSync("missing.temp.txt", missing);
