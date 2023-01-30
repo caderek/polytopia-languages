cbb3ca3a043c7ec8668179f902c64d0b3d393890
Maciej Caderek <maciej.caderek@gmail.com>
2023-01-30 21:04:57 +0100

    Updated main page

--- scripts/form.js
+++ scripts/form.js
@@ -1,48 +1,68 @@
-const { readFileSync } = require("fs")
+const { readFileSync } = require("fs");
+
+const html = String.raw;
 
 const prepareCountries = () => {
   const content = readFileSync("assets/countries.txt")
     .toString()
     .split("\n")
-    .filter((line) => line.trim() !== "")
+    .filter((line) => line.trim() !== "");
 
-  let isOdd = false
-  const countries = []
+  let isOdd = false;
+  const countries = [];
 
   for (const line of content) {
     if (!isOdd) {
-      countries.push({ short: line, full: null })
+      countries.push({ short: line, full: null });
     } else {
-      countries[countries.length - 1].full = line
+      // @ts-ignore
+      countries[countries.length - 1].full = line;
     }
 
-    isOdd = !isOdd
+    isOdd = !isOdd;
   }
 
-  return countries
-}
+  return countries;
+};
 
 const form = () => {
-  const countries = prepareCountries()
+  const countries = prepareCountries();
 
-  return `
-  <form id="add-translation" class="add-form" name="add-translation"  method="POST" data-netlify-recaptcha="true" netlify>
-    <select id="country" name="country">
-      <option disabled selected>Select country (language flag)</option>
-      ${countries
-        .map(
-          ({ short, full }) =>
-            `<option value="${short}">${short} - ${full}</option>`,
-        )
-        .join("\n")}
-    </select>
-    <input type="text" placeholder="Author" required name="author" />
-    <textarea name="translation" required rows="10" name="translation" placeholder="Paste your translation here..."></textarea>
-    <textarea rows="3" name="details" placeholder="Additional details (optional)"></textarea>
-    <div class="captcha" data-netlify-recaptcha="true"></div>
-    <button type="submit">ADD TRANSLATION</button>
-  </form>
-  `
-}
+  return html`
+    <form
+      id="add-translation"
+      class="add-form"
+      name="add-translation"
+      method="POST"
+      data-netlify-recaptcha="true"
+      netlify
+    >
+      <select id="country" name="country">
+        <option disabled selected>Select country (language flag)</option>
+        ${countries
+          .map(
+            ({ short, full }) =>
+              `<option value="${short}">${short} - ${full}</option>`
+          )
+          .join("\n")}
+      </select>
+      <input type="text" placeholder="Author" required name="author" />
+      <textarea
+        name="translation"
+        required
+        rows="10"
+        name="translation"
+        placeholder="Paste your translation here..."
+      ></textarea>
+      <textarea
+        rows="3"
+        name="details"
+        placeholder="Additional details (optional)"
+      ></textarea>
+      <div class="captcha" data-netlify-recaptcha="true"></div>
+      <button type="submit">ADD TRANSLATION</button>
+    </form>
+  `;
+};
 
-module.exports = form
+module.exports = form;
--- scripts/render.js
+++ scripts/render.js
@@ -1,61 +1,102 @@
 const fs = require("fs");
 const form = require("./form");
+const html = String.raw;
 
 const render = (data) => {
   const ISODate = new Date().toISOString();
   const date = `${ISODate.slice(0, 10)} ${ISODate.slice(11, 16)}`;
-  const content = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>The Battle of Polytopia translations</title>
-  </head>
-  <body>
-    <img src="assets/logo.png" alt="log" class="logo" />
-    <h2>USER-MADE</h2>
-    <h1>TRANSLATIONS</h1>
-    <div class="flags">${data
-      .map(({ shortLang, language }) => {
-        const src =
-          shortLang === "eo"
-            ? "/assets/flags/eo.svg"
-            : `https://purecatamphetamine.github.io/country-flag-icons/3x2/${shortLang.toUpperCase()}.svg`;
 
-        return `<a href="#${shortLang}"><img src="${src}" title="${language}"></a>`;
-      })
-      .join("\n")}</div>
-    <ul id="translations">${data
-      .map(
-        ({ language, shortLang, author, editors, file, completed }) =>
-          `<li id="${shortLang}"><h3>${shortLang.toUpperCase()} (${language}) <span class="author">by ${
-            author !== undefined ? author : "Anonymous"
-          }</span>${
-            editors ? `<span class="author">, edited by ${editors}</span>` : ""
-          }</h3>
+  const content = html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style.css" />
+        <title>The Battle of Polytopia translations</title>
+      </head>
+      <body>
+        <img src="assets/logo.png" alt="log" class="logo" />
+        <h2>USER-MADE</h2>
+        <h1>TRANSLATIONS</h1>
+        <div class="flags">
+          ${data
+            .map(({ shortLang, language }) => {
+              const src =
+                shortLang === "eo"
+                  ? "/assets/flags/eo.svg"
+                  : `https://purecatamphetamine.github.io/country-flag-icons/3x2/${shortLang.toUpperCase()}.svg`;
+
+              return `<a href="#${shortLang}"><img src="${src}" title="${language}"></a>`;
+            })
+            .join("\n")}
+        </div>
+        <ul id="translations">
+          ${data
+            .map(
+              ({ language, shortLang, author, editors, file, completed }) =>
+                `<li id="${shortLang}"><h3>${shortLang.toUpperCase()} (${language}) <span class="author">by ${
+                  author !== undefined ? author : "Anonymous"
+                }</span>${
+                  editors
+                    ? `<span class="author">, edited by ${editors}</span>`
+                    : ""
+                }</h3>
             <h4>Links (click to copy):</h4>
             <pre class="code">https://polytopia.netlify.app/translations/${file}</pre>
             <pre class="code">https://raw.githubusercontent.com/caderek/polytopia-languages/master/translations/${file}</pre>
             <p class="completed">${completed}% complete</p>
             <a class="preview" href="https://polytopia.netlify.com/${shortLang}">PREVIEW</a>
           </li>`
-      )
-      .join("\n")}</ul>
-      <footer>
-        <p class="question">Want to add a new translation or improve an existing one?</p>
-        <p>Grab the <a href="http://midjiwan.com/lang/en_US.json">original translation</a> and make a pull request on <a href="https://github.com/caderek/polytopia-languages">Github</a> or ping me on <a href="https://www.reddit.com/user/kap89">Reddit</a>.</p>
-        <p>You can also submit your translation via the form below (it will appear on the website when verified):</p>
-        ${form()}
-        <p class="copyright">Created by Maciej Cąderek | Last update: ${date} UTC</p>
-      </footer>
+            )
+            .join("\n")}
+        </ul>
+        <footer>
+          <p class="question">
+            Want to add a new translation or improve an existing one?
+          </p>
+          <p>
+            Grab the
+            <a
+              href="https://raw.githubusercontent.com/caderek/polytopia-languages/master/template.json"
+              target="_blank"
+              >empty template file</a
+            >
+            or
+            <a
+              href="https://github.com/caderek/polytopia-languages/tree/master/templates"
+              >one of the existing templates</a
+            >, and make a pull request on
+            <a
+              href="https://github.com/caderek/polytopia-languages/new/master/templates"
+              target="_blank"
+              >Github</a
+            >
+            or ping me on
+            <a href="https://www.reddit.com/user/kap89">Reddit</a>.
+          </p>
+          <p>
+            Instructions how to use the template:
+            <a
+              href="https://github.com/caderek/polytopia-languages#structure-of-the-template-file"
+              >Structure of the template file</a
+            >
+          </p>
+          <p>
+            You can also submit your translation via the form below (it will
+            appear on the website when verified):
+          </p>
+          ${form()}
+          <p class="copyright">
+            Created by Maciej Cąderek | Last update: ${date} UTC
+          </p>
+        </footer>
 
-    <div id="copied" class="copied hidden">COPIED!</div>
-    <script src="script.js"></script>
-  </body>
-</html>
-`;
+        <div id="copied" class="copied hidden">COPIED!</div>
+        <script src="script.js"></script>
+      </body>
+    </html>
+  `;
 
   fs.writeFileSync("index.html", content);
 };
